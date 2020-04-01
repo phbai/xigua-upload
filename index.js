8eed5539f289d1a258f710eddc5f290f688d073b
baibing <baibing@blacklake.cn>
2020-04-01 13:57:43 +0800

    实现功能

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const fs = require("fs");
 const crc32 = require("buffer-crc32");
 const randomStr = require("simple-rand-str");
 
-const accessKey = "cfc067bb39feff592af82085b42e6dc3";
-const uploadToken =
-  "HMAC-SHA1:1.0:1585654920:cfc067bb39feff592af82085b42e6dc3:CLSjPp3GqB6dE7t9XHWuWEdEujs=";
 const BUFFER_SIZE = 2 * 1024 * 1024;
 
 // const uploadParams =
@@ -20,8 +17,6 @@ const getToken = () => {
         }
       },
       (error, response, body) => {
-        console.error("error:", error); // Print the error if one occurred
-        console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
         const result = JSON.parse(body);
 
         if (!result.data) {
@@ -34,7 +29,7 @@ const getToken = () => {
   });
 };
 
-const getUploadParams = fileSize => {
+const getUploadParams = (fileSize, uploadToken, accessKey) => {
   return new Promise(resolve => {
     request(
       {
@@ -47,7 +42,6 @@ const getUploadParams = fileSize => {
         }
       },
       (error, response, body) => {
-        console.log("body: ", body);
         const result = JSON.parse(body);
 
         if (!result.data) {
@@ -89,11 +83,11 @@ const getFileInfo = name => {
   });
 };
 
-const getUploadId = (tosHost, oid, tosSign) => {
+const getUploadId = (url, tosSign) => {
   return new Promise(resolve => {
     request.post(
       {
-        url: `https://${tosHost}/${oid}?uploads`,
+        url: `https://${url}?uploads`,
         headers: {
           Authorization: tosSign
         }
@@ -111,15 +105,112 @@ const getUploadId = (tosHost, oid, tosSign) => {
   });
 };
 
-const uploadBlock = () => {
-  request.post({
-    url: ""
+const uploadBlock = (url, index, uploadId, tosSign, block) => {
+  return new Promise(resolve => {
+    const requestUrl = `https://${url}?partNumber=${index +
+      1}&uploadID=${uploadId}`;
+
+    request.put(
+      {
+        url: requestUrl,
+        headers: {
+          Authorization: tosSign,
+          "Content-CRC32": crc32
+            .unsigned(block)
+            .toString(16)
+            .padStart(8, "0")
+        },
+        body: block
+      },
+      (error, response, body) => {
+        const result = JSON.parse(body);
+        resolve(result);
+
+        console.log(
+          `上传block${index + 1} url = ${requestUrl}, crc32 = ${crc32
+            .unsigned(block)
+            .toString(16)
+            .padStart(8, "0")}成功`
+        );
+      }
+    );
   });
 };
 
+const confirmAllBlocks = (url, buffers, uploadId, tosSign) => {
+  return new Promise(resolve => {
+    const body = buffers
+      .map(
+        (buffer, index) =>
+          `${index + 1}:${crc32
+            .unsigned(buffer)
+            .toString(16)
+            .padStart(8, "0")}`
+      )
+      .join(",");
+    console.log("body: ", body);
+    request.post(
+      {
+        url: `https://${url}?uploadID=${uploadId}`,
+        headers: {
+          Authorization: tosSign
+        },
+        body
+      },
+      (error, response, body) => {
+        console.log("error: ", error, "body: ", body);
+        const result = JSON.parse(body);
+
+        if (!result.payload) {
+          console.log("校验所有分片失败");
+          process.exit(2);
+        }
+        resolve(result);
+      }
+    );
+  });
+};
+
+const updateVideoInfo = (edgeInfo, fileSize, uploadToken, accessKey) => {
+  return new Promise(resolve => {
+    request.post(
+      {
+        url: `https://vas-lf-x.snssdk.com/video/openapi/v1/?action=UpdateVideoUploadInfos&vidc=lf&vts=1585641839017020497&host=${edgeInfo.tos_host}&region=CN&province=31&edge_node=lf&upload_mode=serial&file_size=${fileSize}&strategy=long_memory_filter_v2&user_ip=114.86.88.205`,
+        headers: {
+          authorization: uploadToken,
+          "x-tt-access": accessKey
+        },
+        json: {
+          vid: edgeInfo.vid,
+          oid: edgeInfo.oid,
+          token: edgeInfo.token,
+          poster_ss: 0,
+          is_exact_poster: false,
+          user_reference: ""
+        }
+      },
+      (error, response, body) => {
+        console.log("error: ", error, "body: ", body);
+
+        if (!body.data) {
+          console.log("更新视频信息错误");
+          process.exit(2);
+        }
+        resolve(body);
+      }
+    );
+  });
+};
 const main = async () => {
-  const fileInfo = await getFileInfo("switch.mp4");
-  const buffers = await convertFileToBuffers("switch.mp4");
+  if (process.argv.length < 3) {
+    console.log("node index.js [filename]");
+    return;
+  }
+
+  const filename = process.argv[2];
+
+  const fileInfo = await getFileInfo(filename);
+  const buffers = await convertFileToBuffers(filename);
   console.log(
     "buffers: ",
     buffers.map(buffer => buffer.length)
@@ -127,43 +218,53 @@ const main = async () => {
 
   console.log(
     "crc-32:",
-    buffers.map(buffer => crc32.unsigned(buffer).toString(16))
+    buffers.map(buffer =>
+      crc32
+        .unsigned(buffer)
+        .toString(16)
+        .padStart(8, "0")
+    )
   );
 
-  // const token = await getToken();
-  // const params = await getUploadParams(fileInfo.size);
-  // var params = `{"code":2000,"message":"ok","data":{"vid":"v02004e20000bq1ees7l54d8pnfnkpcg","oid":"tos-cn-v-0000/c747f530cce14178bbf91a239f1302cc","tos_host":"tos-lf-x.snssdk.com","tos_up_hosts":[],"tos_headers":{},"tos_sign":"BLZ40F8TCN3JNCL5MVY5:ZCA_kT_K9o89glZOzjAf9ziqmsJO2VI3YOBz9CL8yFQ=:ZGVhZGxpbmU6IDE1ODU3MjM2MzI=:Yzc0N2Y1MzBjY2UxNDE3OGJiZjkxYTIzOWYxMzAyY2M=","token":"eyJob3N0IjoidG9zLWxmLXguc25zc2RrLmNvbSIsIm5vbmNlIjoiUmpuaGZhdHkiLCJ1cGxvYWRfc2lnbiI6IkJMWjQwRjhUQ04zSk5DTDVNVlk1OlpDQV9rVF9LOW84OWdsWk96akFmOXppcW1zSk8yVkkzWU9CejlDTDh5RlE9OlpHVmhaR3hwYm1VNklERTFPRFUzTWpNMk16ST06WXpjME4yWTFNekJqWTJVeE5ERTNPR0ppWmpreFlUSXpPV1l4TXpBeVkyTT0ifQ==:e6cfe7a2fa4878d85f2fe46c56c846e01b07c7aae2c96b4f43f032c3dc95f96a","extra_param":"vidc=lf\u0026vts=1585637232802733807\u0026host=tos-lf-x.snssdk.com\u0026region=CN\u0026province=31\u0026edge_node=lf\u0026upload_mode=serial\u0026file_size=917394.000000\u0026strategy=idc_filter\u0026user_ip=114.86.88.205","bucket":"tos-cn-v-0000","access_key":"BLZ40F8TCN3JNCL5MVY5","dns_info":null,"upload_mode":"serial","edge_nodes":[{"vid":"v02004e20000bq1ees7l54d8pnfnkpcg","oid":"tos-cn-v-0000/570155ef01a24fe2b55ddbd527246a35","tos_host":"tos-hl-x.snssdk.com","tos_up_hosts":[],"tos_headers":{},"tos_sign":"BLZ40F8TCN3JNCL5MVY5:BWOmo-kmXeN_qF8yKAwBDfgg9TImy5_yG-NJHqwby2Y=:ZGVhZGxpbmU6IDE1ODU3MjM2MzI=:NTcwMTU1ZWYwMWEyNGZlMmI1NWRkYmQ1MjcyNDZhMzU=","token":"eyJob3N0IjoidG9zLWhsLXguc25zc2RrLmNvbSIsIm5vbmNlIjoieGhBRHRQRlIiLCJ1cGxvYWRfc2lnbiI6IkJMWjQwRjhUQ04zSk5DTDVNVlk1OkJXT21vLWttWGVOX3FGOHlLQXdCRGZnZzlUSW15NV95Ry1OSkhxd2J5Mlk9OlpHVmhaR3hwYm1VNklERTFPRFUzTWpNMk16ST06TlRjd01UVTFaV1l3TVdFeU5HWmxNbUkxTldSa1ltUTFNamN5TkRaaE16VT0ifQ==:30c24cd760cf70328e101a9249ef8efdb56656437bab3422d6b1a3958200c4ec","extra_param":"vidc=lf\u0026vts=1585637232802733807\u0026host=tos-hl-x.snssdk.com\u0026region=CN\u0026province=31\u0026edge_node=hl\u0026upload_mode=serial\u0026file_size=917394.000000\u0026strategy=long_memory_filter_v2\u0026user_ip=114.86.88.205","bucket":"tos-cn-v-0000","access_key":"BLZ40F8TCN3JNCL5MVY5","dns_info":null,"upload_mode":"serial"}],"delay_upload":0,"trace_id":"2e174636-25d6-488f-9098-47b4dfd659f9","upload_id":"74b2567af0614ef7bac965119b74df4e"}}`;
+  const token = await getToken();
+  const { accessKey, uploadToken } = token.data;
+  const params = await getUploadParams(fileInfo.size, uploadToken, accessKey);
 
-  // console.log("params: ", params);
-  // console.log("edge_nodes: ", params.data.edge_nodes);
-  const edge_nodes = [
-    {
-      vid: "v02004880000bq1hf5qepr109a254k5g",
-      oid: "tos-cn-v-0000c001/294ce781d9c245f18b773fa4aa91112b",
-      tos_host: "tos-nc2-slb1.bytecdn.cn",
-      tos_up_hosts: [],
-      tos_headers: {},
-      tos_sign:
-        "DT2GDTBW6QYYT95M50B1:hyd0xErSzkOvruAqafSnX2GrPr59CXTr37X6KsOuSq8=:ZGVhZGxpbmU6IDE1ODU3MzU5NTk=:Mjk0Y2U3ODFkOWMyNDVmMThiNzczZmE0YWE5MTExMmI=",
-      token:
-        "eyJob3N0IjoidG9zLW5jMi1zbGIxLmJ5dGVjZG4uY24iLCJub25jZSI6ImlMV2dqUEFWIiwidXBsb2FkX3NpZ24iOiJEVDJHRFRCVzZRWVlUOTVNNTBCMTpoeWQweEVyU3prT3ZydUFxYWZTblgyR3JQcjU5Q1hUcjM3WDZLc091U3E4PTpaR1ZoWkd4cGJtVTZJREUxT0RVM016VTVOVGs9Ok1qazBZMlUzT0RGa09XTXlORFZtTVRoaU56Y3pabUUwWVdFNU1URXhNbUk9In0=:cc38057ceb64d89505466d2f7b9462d154ef3d78d1f07b1290d0b60d5ec99357",
-      extra_param:
-        "vidc=lf&vts=1585649559835574552&host=tos-nc2-slb1.bytecdn.cn&region=CN&province=31&edge_node=lf&upload_mode=serial&file_size=5041902.000000&strategy=long_memory_filter_v2&user_ip=114.86.88.205",
-      bucket: "tos-cn-v-0000c001",
-      access_key: "DT2GDTBW6QYYT95M50B1",
-      dns_info: null,
-      upload_mode: "serial"
-    }
-  ];
+  console.log("edge_nodes: ", params.data.edge_nodes);
+  const edge_nodes = params.data.edge_nodes;
 
-  // const uploadIdReponse = await getUploadId(
-  //   edge_nodes[0].tos_host,
-  //   edge_nodes[0].oid,
-  //   edge_nodes[0].tos_sign
-  // );
-  // const uploadId = uploadIdReponse.payload.uploadID;
-  const uploadId = "dfa28136-88b5-457e-b895-62fd75472e38";
+  const uploadIdReponse = await getUploadId(
+    `${edge_nodes[0].tos_host}/${edge_nodes[0].oid}`,
+    edge_nodes[0].tos_sign
+  );
+  const uploadId = uploadIdReponse.payload.uploadID;
   console.log("uploadId: ", uploadId);
+
+  for (let i = 0; i < buffers.length; i++) {
+    await uploadBlock(
+      `${edge_nodes[0].tos_host}/${edge_nodes[0].oid}`,
+      i,
+      uploadId,
+      edge_nodes[0].tos_sign,
+      buffers[i]
+    );
+  }
+  await confirmAllBlocks(
+    `${edge_nodes[0].tos_host}/${edge_nodes[0].oid}`,
+    buffers,
+    uploadId,
+    edge_nodes[0].tos_sign
+  );
+  const videoInfo = await updateVideoInfo(
+    edge_nodes[0],
+    fileInfo.size,
+    uploadToken,
+    accessKey
+  );
+  console.log(
+    "上传成功 链接：",
+    `https://p3.pstatp.com/origin/${videoInfo.data.video.oid}`
+  );
 };
 
 main();
